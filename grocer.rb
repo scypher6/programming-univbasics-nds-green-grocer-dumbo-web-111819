6a7df06b5712366970d103a954c6b1d7ca3f690e
Joseph Pyram <joseph.pyram@gmail.com>
2019-11-12 23:42:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,45 @@
+require "pry"
+
 def find_item_by_name_in_collection(name, collection)
+  #binding.pry
   # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  collection.each do |hash|
+    return hash if hash[:item] == name
+  end
+    nil
 end
+
+
+#Finds all dups in an AoH and returns a H
+def find_dups(cart)
+  cart.group_by {|e| e}.select { |k, v| v.size > 1}.map {|k, v| [k.values[0], v.size] }.to_h
+end
+
+#find_item_by_name_in_collection("bread", cart)
 
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  count = 1
+  #get cart with unique values
+  u_cart = cart.uniq {|e| e[:item] }
+  #get all duplicates
+  dups = find_dups(cart)
+  #Add correct count
+  i = 0     #index for dups array
+  u_cart.size.times do |index|
+    if (!dups.empty? && (dups.keys[i] == u_cart[index][:item]))   #duplicate found
+        u_cart[index][:count] = dups.values[i]
+        i += 1
+    else
+        u_cart[index][:count] = count
+    end  
+  end
+  u_cart
 end
+
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
